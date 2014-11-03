f9e184cd128b7807b8fc4e54fa794d6703ca9b79
zarghol <clement.nonn@gmail.com>
2014-11-03 17:38:55 +0100

    ajout commentaire dans Agent + Position + Reformage de la grille

--- Agent.java
+++ Agent.java
@@ -6,14 +6,12 @@ import java.util.ArrayList;
 public class Agent extends Thread {
 	ArrayList<Agent> agents;
 	Grille puzzle;
-	int positionX;
-	int positionY;
-	int positionXBut;
-	int positionYBut;
+	private Position position;
+	private Position positionBut;
 	
 	
 	public void communiquer() {
-		
+		// envoyer message
 	}
 	
 	public void percevoirEnvironnement() {
@@ -31,6 +29,26 @@ public class Agent extends Thread {
 		// // traiter messages
 		// // verifier
 		// // raisonne
+		// // // si case vers son chemin est libre
+		// // // // in se déplace
+		// // // sinon
+		// // // // on envoit un message
 		// // effectuer les actions
+	}
+
+	Position getPositionBut() {
+		return positionBut;
+	}
+
+	void setPositionBut(Position positionBut) {
+		this.positionBut = positionBut;
+	}
+
+	Position getPosition() {
+		return position;
+	}
+
+	void setPosition(Position position) {
+		this.position = position;
 	}
 }
--- Grille.java
+++ Grille.java
@@ -1,7 +1,18 @@
 package SMATP3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Grille {
-	private Agent[][] tab;
+	private HashMap<Position, Agent> tab;
+	private int tailleLigne;
 	
+	
+	public ArrayList<Agent> getNeighbourhood(Agent agent) {
+		ArrayList<Agent> result = new ArrayList<Agent>();
+		Position p1 = agent.getPosition();
+		result.add(this.tab.get())
+		return null;
+	}
 }

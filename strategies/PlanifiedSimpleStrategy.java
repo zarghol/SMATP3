0001cabaaf6041f4bd6cd125e1bb565c710e7123
zarghol <clement.nonn@gmail.com>
2014-11-14 15:10:58 +0100

    toujours plus de stratégies
    création des deux stratégies de base sans les messages

---
+++ strategies/PlanifiedSimpleStrategy.java
@@ -0,0 +1,42 @@
+package SMATP3.strategies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import SMATP3.Agent;
+import SMATP3.Position;
+import SMATP3.messages.Message;
+
+public class PlanifiedSimpleStrategy implements ThinkingStrategy {
+	private List<Position> route;
+
+	@Override
+	public void reflexionAction(Agent agent) {
+		Position nextPosition = route.remove(0);
+		if (route == null || agent.getSnapshot().isPositionOccupied(nextPosition)) {
+			// calcul itinéraire
+			this.findRoute();
+		} 
+		// on avance
+		agent.move(nextPosition);
+		
+	}
+
+	@Override
+	public String getName() {
+		return "PlanifiedSimpleStrategy";
+	}
+
+	@Override
+	public void handleMessage(Message message, Agent agent) {
+	}
+	
+	private void findRoute() {
+		List<Position> route = new ArrayList<Position>();
+		// TODO: find route (djikstra, etc..)
+		
+		
+		this.route = route;
+	}
+
+}
